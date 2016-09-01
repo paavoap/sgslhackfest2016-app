bf81da5cfa1726ed1d85d960dd62d3ae6f2da5c5
Paavo Parkkinen <paavoap@sg.ibm.com>
2016-09-01 16:03:25 +0800

    Heat map from DB.

--- app.js
+++ app.js
@@ -16,6 +16,30 @@ var cfenv = require('cfenv');
 var rp = require('request-promise');
 var moment = require('moment-timezone');
 
+var ibmdb = require('ibm_db');
+var db2;
+var hasConnect = false;
+
+if (process.env.VCAP_SERVICES) {
+  var env = JSON.parse(process.env.VCAP_SERVICES);
+	if (env['dashDB']) {
+    hasConnect = true;
+		db2 = env['dashDB'][0].credentials;
+	}
+}
+
+if ( hasConnect == false ) {
+  db2 = {
+    db: "BLUDB",
+    hostname: "dashdb-entry-yp-dal09-07.services.dal.bluemix.net",
+    port: 50000,
+    username: "dash7927",
+    password: "0ccUVgDpsy8l"
+  };
+}
+
+var connString = "DRIVER={DB2};DATABASE=" + db2.db + ";UID=" + db2.username + ";PWD=" + db2.password + ";HOSTNAME=" + db2.hostname + ";port=" + db2.port;
+
 // create a new express server
 var app = express();
 
@@ -37,9 +61,9 @@ function parseResponse(res) {
 
 function getPrediction(ts, lat, lng) {
   var m = moment(ts);
-  var sg = m.clone().tz("Asia/Singapore")
+  var sg = m.clone().tz("Asia/Singapore");
   // Wed Aug 17 2016 16:37:18 GMT+0800 (SGT)
-  var tss = sg.format("ddd MMM D YYYY HH:mm:ss [GMT]ZZ [(SGT)]")
+  var tss = sg.format("ddd MMM D YYYY HH:mm:ss [GMT]ZZ [(SGT)]");
   var body = {
       "arguments": [ ['"'+tss+'"', lng, lat].join(',') ]
   };
@@ -53,7 +77,7 @@ function getPrediction(ts, lat, lng) {
     },
     body: body,
     json: true
-  }
+  };
   return rp(options)
     .then(function(res) {
       try {
@@ -68,6 +92,36 @@ function getPrediction(ts, lat, lng) {
     });
 }
 
+function getTaxisFromDB(ibmdb, connString, ts, success, fail) {
+  ibmdb.open(connString, function(err, conn) {
+    if (err) {
+      console.log(err);
+      fail(err);
+    } else {
+      var q = "SELECT LAT, LNG FROM DASH7927.TAXI_LOCATIONS TABLESAMPLE BERNOULLI(1) WHERE HOUR(TIMESTAMP) = HOUR(date('"+ts+"')) LIMIT 1000;";
+      conn.query(q, function (err, points, moreResultSets) {
+        if (err) {
+          console.log(err);
+          fail(err);
+        } else {
+          success(points);
+        }
+      });
+      conn.close(function() { return; });
+    }
+  });
+}
+
+function getTaxis(ts) {
+  var m = moment(ts);
+  var sg = m.clone().tz("Asia/Singapore");
+  // 2016-08-17 16:37:18 GMT+0800 (SGT)
+  var tss = sg.format("YYYY-MM-DD HH:mm:ss");
+  return new Promise(function (resolve, reject) {
+    getTaxisFromDB(ibmdb, connString, tss, resolve, reject);
+  });
+}
+
 app.post('/api/predict', function (req, res) {
   var tss = req.body.timestamps;
   // TODO: check tss type == Array
@@ -77,6 +131,15 @@ app.post('/api/predict', function (req, res) {
   Promise.all(ps)
     .then(function(cs) {
       res.json(cs);
+    });
+});
+
+app.post('/api/heatmap', function (req, res) {
+  var ts = req.body.timestamp;
+  // TODO: check tss type == Array
+  getTaxis(new Date(ts))
+    .then(function(ps) {
+      res.json(ps);
     });
 });
 
--- public/javascript/map.js
+++ public/javascript/map.js
@@ -1,102 +1,41 @@
-var maxLng = 104.0168;
-var minLng = 103.61368;
-var maxLat = 1.46989;
-var minLat = 1.23377;
-var cellH = 0.02;
-var cellW = 0.02;
-
-function getPrediction(ts, lat, lng) {
+function getPoints(ts) {
   var req = {
-    lat: lat,
-    lng: lng,
-    timestamps: [
-      ts
-    ]
+    timestamp: ts
   };
   return new Promise(function(resolve, reject) {
     $.ajax({
       type: "POST",
-      url: "/api/predict",
+      url: "/api/heatmap",
       contentType: "application/json; charset=utf-8",
       data: JSON.stringify(req),
     }).fail(function () {
-      var error = "Request to /api/predict failed.";
+      var error = "Request to /api/heatmap failed.";
       console.log(error);
       reject(error);
     }).done(function (data) {
-      resolve({
-        lat: lat,
-        lng: lng,
-        val: data[0]
-      });
+      resolve(data);
     });
   });
-}
-
-function getPredictions(ts, latlngs) {
-  var ps = latlngs.map(function(pair) {
-    var lat = pair[0];
-    var lng = pair[1];
-    return getPrediction(ts, lat, lng);
-  })
-  return Promise.all(ps);
 }
 
 function renderHeatmap(map, data) {
-  data.forEach(function(d) {
-    renderHeatmapCell(map, d);
+  var points = data.map(function(p) {
+    // lat/lng is flipped in the DB
+    var lat = p.LNG;
+    var lng = p.LAT;
+    return new google.maps.LatLng(lat, lng);
+  })
+  heatmap = new google.maps.visualization.HeatmapLayer({
+    data: points,
+    map: map
   });
-}
-
-function renderHeatmapCell(map, data) {
-  console.log(data);
-
-  var value = data.val;
-  if (value == null) {
-    return;
-  }
-  if (value < 0 || 1 < value) {
-    return;
-  }
-
-  var lat = data.lat;
-  var lng = data.lng;
-  var cellCoords = [
-    { lat: lat, lng: lng },
-    { lat: lat+cellH, lng: lng },
-    { lat: lat+cellH, lng: lng+cellW },
-    { lat: lat, lng: lng+cellW },
-  ];
-
-  var cell = new google.maps.Polygon({
-    paths: cellCoords,
-    strokeColor: '#FF0000',
-    strokeOpacity: 0.8,
-    strokeWeight: 1,
-    fillColor: '#FF0000',
-    fillOpacity: value
-  });
-  cell.setMap(map);
 }
 
 function drawHeatmap(map, ts) {
-  var pairs = [];
-
-  var cLat = minLat;
-  while (cLat < maxLat) {
-    var cLng = minLng;
-    while (cLng < maxLng) {
-      pairs.push([cLat, cLng]);
-      cLng = cLng + cellW;
-    }
-    cLat = cLat + cellH;
-  }
-
-  getPredictions(ts, pairs)
+  getPoints(ts)
     .then(function (data) {
       renderHeatmap(map, data);
     });
-
 }
 
 function initMap() {
@@ -125,8 +64,3 @@ function initMap() {
   drawHeatmap(map, date);
 
 }
-
-$(function() {
-    console.log( "Ready!" );
-
-});
